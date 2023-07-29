759722e61a168c57526dac5bfed53c52b599b667
amanbind898 <aman.2201086cs@iiitbh.ac.in>
2023-07-29 14:04:07 +0530

    hamburger icon

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const e = document.querySelector(".header__main-ham-menu-cont")
-  , n = document.querySelector(".header__sm-menu")
-  , s = document.querySelector(".header__main-ham-menu")
-  , t = document.querySelector(".header__main-ham-menu-close")
-  , a = document.querySelectorAll(".header__sm-menu-link");
+const e = document.querySelector(".header-main-ham-menu-cont")
+  , n = document.querySelector(".hamburger-menu")
+  , s = document.querySelector(".header-main-ham-menu")
+  , t = document.querySelector(".header-main-ham-menu-close")
+  , a = document.querySelectorAll(".hamburger-menu-link");
 e.addEventListener("click", ()=>{
-    n.classList.contains("header__sm-menu--active") ? n.classList.remove("header__sm-menu--active") : n.classList.add("header__sm-menu--active"),
+    n.classList.contains("hamburger-menu--active") ? n.classList.remove("hamburger-menu--active") : n.classList.add("hamburger-menu--active"),
     s.classList.contains("d-none") ? (s.classList.remove("d-none"),
     t.classList.add("d-none")) : (s.classList.add("d-none"),
     t.classList.remove("d-none"))
@@ -12,12 +12,12 @@ e.addEventListener("click", ()=>{
 );
 for (let e = 0; e < a.length; e++)
     a[e].addEventListener("click", ()=>{
-        n.classList.remove("header__sm-menu--active"),
+        n.classList.remove("hamburger-menu--active"),
         s.classList.remove("d-none"),
         t.classList.add("d-none")
     }
     );
-const c = document.querySelector(".header__logo-container");
+const c = document.querySelector(".header-logo-container");
 c.addEventListener("click", ()=>{
     location.href = "index.html"
 }
