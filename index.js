c1e23f6e35c221a1b645f2f939e65be174d68cb4
amanbind898 <aman.2201086cs@iiitbh.ac.in>
2024-06-01 12:57:38 +0530

    name

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 let collegeData = [];
 
-const csvFilePath = path.join(__dirname, 'NITall.csv'); 
+const csvFilePath = path.join(__dirname, 'NIT.csv'); 
 
 fs.createReadStream(csvFilePath)
   .pipe(csv())
